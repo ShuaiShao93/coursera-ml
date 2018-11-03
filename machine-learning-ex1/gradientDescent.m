94b37b4c991b898f294bb35bec42ab10747dfdb0
Shuai Shao <2bsunshine4u@gmail.com>
2018-11-03 11:42:49 -0700

    hw2

--- machine-learning-ex1/gradientDescent.m
+++ machine-learning-ex1/gradientDescent.m
@@ -16,12 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    theta = theta - alpha * 1/m * (X' * (X * theta - y));
-
-
-
-
-
+    
+    delta = 0;
+    for i = 1:m
+      delta += (X(i, :)*theta - y(i))*X(i, :)';
+    endfor
+    theta = theta - alpha / m * delta;
 
     % ============================================================
 
